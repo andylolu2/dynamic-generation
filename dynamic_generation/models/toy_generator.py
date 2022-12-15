91b897658ff3340795ed02e6f6f694c46cd92739
andylolu2 <andylolu24@gmail.com>
2022-12-15 15:43:39 +0800

    Bug fix: correct event shape

--- dynamic_generation/models/toy_generator.py
+++ dynamic_generation/models/toy_generator.py
@@ -1,8 +1,9 @@
 import torch
+import torch.distributions as D
 from torch import nn
-from torchtyping import TensorType
 
 from dynamic_generation.experiments.utils.metrics import global_metrics
+from dynamic_generation.types import Tensor
 from dynamic_generation.utils.stability import safe_log
 
 
@@ -28,20 +29,23 @@ class ToyGenerator(nn.Module):
             nn.Linear(hidden_dim, output_dim),
         )
 
-    def forward(self, z: TensorType["batch", "hidden"]):
-        return self.generator(z)
+    def forward(self, z: Tensor):
+        mean = self.generator(z)
+        X_hat = D.Independent(D.Normal(mean, self.std), 1)
+        return X_hat
 
     def generate(self, n: int, device):
         z = torch.rand(n, self.z_dim, device=device)
-        return self.forward(z)
+        X_hat = self.forward(z)
+        return X_hat.mean
 
-    def loss(self, x: TensorType["batch", "output"]):
+    def loss(self, x: Tensor):
         z = torch.rand(1, self.z_samples, self.z_dim, device=x.device)
-        x_hat = self.forward(z)
+        X_hat = self.forward(z)
+        X_hat = X_hat.expand((x.shape[0], self.z_samples))
 
-        d = torch.linalg.norm(x.unsqueeze(1) - x_hat, dim=-1)
-        lik_per_z = torch.exp(-0.5 * (d / self.std) ** 2) + 1e-10
-        log_lik = torch.mean(safe_log(lik_per_z.mean(dim=1)))
+        lik_per_z = X_hat.log_prob(x.unsqueeze(1)).exp()
+        log_lik = torch.mean(safe_log(lik_per_z.mean(1)))
         loss = -log_lik
 
         global_metrics.log("loss", loss.item(), "mean")
--- dynamic_generation/models/vae.py
+++ dynamic_generation/models/vae.py
@@ -1,10 +1,13 @@
+from typing import Sequence
+
 import torch
 import torch.distributions as D
 from torch import nn
 from torchtyping import TensorType
 
 from dynamic_generation.experiments.utils.metrics import global_metrics
-from dynamic_generation.models.mlp import MLP
+from dynamic_generation.models.block_sequential import BlockSequential
+from dynamic_generation.models.blocks import LinearBlock
 from dynamic_generation.models.ponder_module import GRUPonderModule, RNNPonderModule
 from dynamic_generation.models.ponder_net import PonderNet
 from dynamic_generation.types import Tensor
@@ -23,23 +26,16 @@ class BaseVAE(nn.Module):
         raise NotImplementedError()
 
     def encode(self, x: Tensor):
-        Z_hat = self.base_encode(x)
-
-        if self.training:
-            z_hat = Z_hat.rsample()
-        else:
-            z_hat = Z_hat.mean
-
-        return {"z_hat": z_hat, "Z_hat": Z_hat}
+        return {"Z_hat": self.base_encode(x)}
 
     def generate(self, n: int, device):
         z = self.prior.sample((n,)).to(device=device)  # type: ignore
         x, aux = self.decode(z)
-        return x.sample(), aux
+        return x.mean, aux
 
     def forward(self, x: Tensor):
         encoded = self.encode(x)
-        X_hat, aux = self.decode(encoded["z_hat"])
+        X_hat, aux = self.decode(encoded["Z_hat"].rsample())
         return {"X_hat": X_hat} | encoded | aux
 
     def loss(self, x: Tensor, out: dict, beta: float = 1):
@@ -72,11 +68,7 @@ class BaseVAE(nn.Module):
 
 class UniformBetaVAE(BaseVAE):
     def __init__(
-        self,
-        z_dim: int,
-        hidden_dim: int,
-        input_dim: int,
-        n_layers: int,
+        self, z_dim: int, hidden_dim: int, input_dim: int, n_layers: int, std: float
     ):
         super().__init__()
 
@@ -88,71 +80,21 @@ class UniformBetaVAE(BaseVAE):
         enc_dims = [input_dim] + [hidden_dim] * (n_layers - 1) + [2 * z_dim]
         dec_dims = [z_dim] + [hidden_dim] * (n_layers - 1) + [input_dim]
 
-        self.encoder = MLP(enc_dims, activation=nn.GELU())
-        self.decoder = MLP(dec_dims, activation=nn.GELU())
+        def block_fn(in_dim, out_dim):
+            return nn.Sequential(nn.Linear(in_dim, out_dim), nn.GELU())
 
-        self.std: Tensor
-        self.register_buffer("std", torch.tensor(0.01))
-        # self.std = nn.parameter.Parameter(torch.tensor(1.0))
+        def last_block_fn(in_dim, out_dim):
+            return nn.Linear(in_dim, out_dim)
 
-        self.register_buffer("prior_low", torch.zeros(z_dim))
-        self.register_buffer("prior_high", torch.ones(z_dim))
-
-    @property
-    def prior(self):
-        return D.Uniform(self.prior_low, self.prior_high)
-
-    def base_encode(self, x: Tensor) -> D.Distribution:
-        h = self.encoder(x).sigmoid() * 400
-        a, b = h.chunk(2, dim=-1)
-        return D.Beta(a, b)
-
-    def decode(self, z: TensorType["batch", "hidden"]):
-        mean = self.decoder(z)
-        cov = self.std**2 * torch.eye(self.input_dim, device=mean.device)
-        X_hat = D.MultivariateNormal(loc=mean, covariance_matrix=cov)
-        return X_hat, {}
-
-
-class DynamicVae(BaseVAE):
-    def __init__(
-        self,
-        z_dim: int,
-        enc_hidden_dim: int,
-        enc_n_layers: int,
-        dec_hidden_dim: int,
-        dec_n_layers: int,
-        input_dim: int,
-        epsilon: float,
-        lambda_p: float,
-        beta: float,
-        N_max: int,
-    ):
-        super().__init__()
-
-        self.z_dim = z_dim
-        self.enc_hidden_dim = enc_hidden_dim
-        self.enc_n_layers = enc_n_layers
-        self.dec_hidden_dim = dec_hidden_dim
-        self.dec_n_layers = dec_n_layers
-        self.input_dim = input_dim
-
-        enc_dims = [input_dim] + [enc_hidden_dim] * (enc_n_layers - 1) + [2 * z_dim]
-        self.encoder = MLP(enc_dims, activation=nn.GELU())
-
-        ponder_module = GRUPonderModule(z_dim, dec_hidden_dim, input_dim, dec_n_layers)
-        # ponder_module = RNNPonderModule(z_dim, dec_hidden_dim, input_dim, dec_n_layers)
-        self.decoder = PonderNet(
-            epsilon=epsilon,
-            lambda_p=lambda_p,
-            beta=beta,
-            N_max=N_max,
-            ponder_module=ponder_module,
+        self.encoder = BlockSequential(
+            enc_dims, block=block_fn, last_block=last_block_fn
+        )
+        self.decoder = BlockSequential(
+            dec_dims, block=block_fn, last_block=last_block_fn
         )
 
         self.std: Tensor
-        self.register_buffer("std", torch.tensor(0.01))
-        # self.std = nn.parameter.Parameter(torch.tensor(1.0))
+        self.register_buffer("std", torch.tensor(std))
 
         self.prior_low: Tensor
         self.prior_high: Tensor
@@ -161,17 +103,80 @@ class DynamicVae(BaseVAE):
 
     @property
     def prior(self):
-        return D.Uniform(self.prior_low, self.prior_high)
+        return D.Independent(D.Uniform(self.prior_low, self.prior_high), 1)
 
-    def base_encode(self, x: Tensor) -> D.Distribution:
+    def base_encode(self, x: Tensor):
         h = self.encoder(x).sigmoid() * 400
         a, b = h.chunk(2, dim=-1)
-        return D.Beta(a, b)
+        return D.Independent(D.Beta(a, b), 1)
+
+    def decode(self, z: TensorType["batch", "hidden"]):
+        mean = self.decoder(z)
+        X_hat = D.Independent(D.Normal(mean, self.std), 1)
+        return X_hat, {}
+
+
+class DynamicVae(BaseVAE):
+    def __init__(
+        self,
+        z_dim: int,
+        enc_dims: Sequence[int],
+        dec_hidden_dim: int,
+        dec_n_layers: int,
+        input_dim: int,
+        epsilon: float,
+        lambda_p: float,
+        beta: float,
+        N_max: int,
+        average_halt_dist: bool,
+        std: float,
+    ):
+        super().__init__()
+
+        self.z_dim = z_dim
+        self.enc_dims = list(enc_dims)
+        self.dec_hidden_dim = dec_hidden_dim
+        self.dec_n_layers = dec_n_layers
+        self.input_dim = input_dim
+
+        enc_dims = [input_dim] + self.enc_dims + [2 * z_dim]
+        self.encoder = BlockSequential(
+            enc_dims,
+            block=LinearBlock(post_block=nn.GELU()),
+            last_block=LinearBlock(),
+        )
+
+        # ponder_module = GRUPonderModule(z_dim, dec_hidden_dim, input_dim, dec_n_layers)
+        ponder_module = RNNPonderModule(z_dim, dec_hidden_dim, input_dim, dec_n_layers)
+        self.decoder = PonderNet(
+            epsilon=epsilon,
+            lambda_p=lambda_p,
+            beta=beta,
+            N_max=N_max,
+            ponder_module=ponder_module,
+            average_halt_dist=average_halt_dist,
+        )
+
+        self.std: Tensor
+        self.register_buffer("std", torch.tensor(std))
+
+        self.prior_low: Tensor
+        self.prior_high: Tensor
+        self.register_buffer("prior_low", torch.zeros(z_dim))
+        self.register_buffer("prior_high", torch.ones(z_dim))
+
+    @property
+    def prior(self):
+        return D.Independent(D.Uniform(self.prior_low, self.prior_high), 1)
+
+    def base_encode(self, x: Tensor):
+        h = self.encoder(x).sigmoid() * 400
+        a, b = h.chunk(2, dim=-1)
+        return D.Independent(D.Beta(a, b), 1)
 
     def decode(self, z: TensorType["batch", "hidden"]):
         means, halt_dist = self.decoder.forward(z)
-        cov = self.std**2 * torch.eye(self.input_dim, device=means.device)
-        X_hats = D.MultivariateNormal(loc=means, covariance_matrix=cov)
+        X_hats = D.Independent(D.Normal(means, self.std), 1)
         halt_dist = halt_dist.expand(X_hats.batch_shape[:-1])
         X_hat = CustomMixture(halt_dist, X_hats)
         return X_hat, {"halt_dist": halt_dist}
@@ -179,7 +184,7 @@ class DynamicVae(BaseVAE):
     def generate(self, n: int, device):
         z = self.prior.sample((n,)).to(device=device)  # type: ignore
         x, aux = self.decode(z)
-        sample, mix_sample = x.sample_detailed()
+        sample, mix_sample = x.sample_detailed(method="mean")
         return sample, aux | {"mix_sample": mix_sample}
 
     def loss(self, x: Tensor, out: dict, beta: float = 1):
