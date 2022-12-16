1a26085baecd89b8b031992376fb2fc65c758a00
andylolu2 <andylolu24@gmail.com>
2022-12-16 16:02:36 +0800

    Add type hints to load_optimizer

--- dynamic_generation/utils/optimizers.py
+++ dynamic_generation/utils/optimizers.py
@@ -1,6 +1,6 @@
 import torch
 
 
-def load_optimizer(name: str, *args, **kwargs):
+def load_optimizer(name: str, *args, **kwargs) -> torch.optim.Optimizer:
     optimizer_cls = getattr(torch.optim, name)
     return optimizer_cls(*args, **kwargs)
