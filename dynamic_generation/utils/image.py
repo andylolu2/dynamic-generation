5e0c4f815c4104ddd8f02a6cd32d98f13ed0c769
andylolu2 <andylolu24@gmail.com>
2023-01-09 22:55:05 +0800

    Allow remapping tensor range for image tiling

--- dynamic_generation/utils/image.py
+++ dynamic_generation/utils/image.py
@@ -5,12 +5,20 @@ import torchvision
 from dynamic_generation.types import Tensor
 
 
-def tile_image(x: Tensor, nrow: int | None = None):
+def tile_image(
+    x: Tensor,
+    nrow: int | None = None,
+    input_range: tuple[float, float] = (0.0, 1.0),
+):
     # x: N,C,H,W
     if nrow is None:
         nrow = math.floor(math.sqrt(x.shape[0]))
     else:
         nrow = x.shape[0] // nrow
+
     # img_grid: C,H',W'
     img_grid = torchvision.utils.make_grid(x, nrow=nrow, pad_value=1.0)
+
+    lo, hi = input_range
+    img_grid = (img_grid - lo) / (hi - lo)  # maps to [0, 1]
     return torchvision.transforms.ToPILImage()(img_grid)
